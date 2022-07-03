c37dbc0b28a4ae4bd372274953497054dd601e56
imamfarisi19 <edwardindo1@gmail.com>
2022-07-04 04:07:28 +0800

    form in guest pages

--- resources/views/guest/templates/form.blade.php
+++ resources/views/guest/templates/form.blade.php
@@ -6,8 +6,29 @@
     <div class="container position-relative">
         <div class="row align-items-center min-vh-75 my-lg-8">
             <div class="col-md-7 col-lg-6 text-center text-md-start py-8">
-                <h1 class="mb-4 display-1 lh-sm">Travel around <br class="d-block d-lg-none d-xl-block" />the world</h1>
-                <p class="mt-4 mb-5 fs-1 lh-base">Plan and book your perfect trip with expert advice, <br class="d-none d-lg-block" />travel tips, destination information and <br class="d-none d-lg-block" />inspiration from us.</p><a class="btn btn-lg btn-primary hover-top" href="#" role="button">Sign Up Now</a>
+            <form action="/action_page.php">
+            <label for="kategori">Kategori Wisata : </label>
+            <br>
+            <select name="kategori" id="kategori">
+              <option value="1"><= 5,9 km</option>
+              <option value="2">6 - 15,9 km</option>
+              <option value="3">16 - 30,9 km</option>
+              <option value="4">>= 31 km</option>
+            </select>
+            <br><br>
+
+            <label for="kelurahan">Kelurahan Sekarang : </label>
+            <br>
+            <select name="kelurahan" id="kelurahan">
+              <option value="1">Rp0 - Rp10.000</option>
+              <option value="2">Rp11.000 - Rp20.999</option>
+              <option value="3">Rp21.000 - Rp50.999</option>
+              <option value="4">>= Rp51.000</option>
+            </select>
+            <br><br>
+            
+            <input type="submit" class="btn btn-primary" value="Submit">
+          </form>
             </div>
         </div>
     </div>
