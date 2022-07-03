34be785c128aca5cf2dd18de0566201639800fad
imamfarisi19 <edwardindo1@gmail.com>
2022-07-04 02:21:52 +0800

    parts from berandaAdmin.blade.php

---
+++ resources/views/templates/script.blade.php
@@ -0,0 +1,5 @@
+<script src="{{asset('AdminLTE-3.2.0/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('AdminLTE-3.2.0/dist/js/adminlte.min.js')}}"></script>
