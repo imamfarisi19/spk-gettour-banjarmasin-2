8d67e7ccc091a05112d32404247b60f9abc6430c
imamfarisi19 <edwardindo1@gmail.com>
2022-07-05 18:27:57 +0800

    form part

--- resources/views/guest/templates/form.blade.php
+++ resources/views/guest/templates/form.blade.php
@@ -1,35 +1,101 @@
 <section class="py-0">
-    <div class="bg-holder d-none d-md-block" style="background-image:url('rhea/public/assets/img/illustrations/form.png');background-position:right bottom;background-size:contain;">
-    </div>
-    <!--/.bg-holder-->
+  <div class="bg-holder d-none d-md-block" style="background-image:url('rhea/public/assets/img/illustrations/form.png');background-position:right bottom;background-size:contain;">
+  </div>
+  <!--/.bg-holder-->
 
-    <div class="container position-relative">
-        <div class="row align-items-center min-vh-75 my-lg-8">
-            <div class="col-md-7 col-lg-6 text-center text-md-start py-8">
-            <form action="/action_page.php">
-            <label for="kategori">Kategori Wisata : </label>
-            <br>
-            <select name="kategori" id="kategori">
-              <option value="1"><= 5,9 km</option>
-              <option value="2">6 - 15,9 km</option>
-              <option value="3">16 - 30,9 km</option>
-              <option value="4">>= 31 km</option>
-            </select>
-            <br><br>
+  <div class="container position-relative">
+    <div class="row align-items-center min-vh-75 my-lg-8">
+      <div class="col-md-7 col-lg-6 text-center text-md-start py-8">
+        <form method="" action="/action_page.php">
+          <label for="kategori">Kategori Wisata : </label>
+          <br>
+          <select name="kategori" id="kategori">
+            <option value="10">Departement Store</option>
+            <option value="8">Gereja</option>
+            <option value="7">Masjid</option>
+            <option value="12">Restoran Non Banjar</option>
+            <option value="4">Tempat Bermain Anak</option>
+            <option value="5">Taman</option>
+            <option value="6">Taman Bermain Air</option>
+          </select>
+          <br><br>
 
-            <label for="kelurahan">Kelurahan Sekarang : </label>
-            <br>
-            <select name="kelurahan" id="kelurahan">
-              <option value="1">Rp0 - Rp10.000</option>
-              <option value="2">Rp11.000 - Rp20.999</option>
-              <option value="3">Rp21.000 - Rp50.999</option>
-              <option value="4">>= Rp51.000</option>
-            </select>
-            <br><br>
-            
-            <input type="submit" class="btn btn-primary" value="Submit">
-          </form>
-            </div>
-        </div>
+          <label for="kelurahan">Kelurahan Sekarang : </label>
+          <br>
+          <select name="kelurahan" id="kelurahan">
+            <option value="1">Basirih</option>
+            <option value="2">Belitung Selatan</option>
+            <option value="3">Belitung Utara</option>
+            <option value="4">Kuin Cerucuk</option>
+            <option value="5">Kuin Selatan</option>
+            <option value="6">Pelambuan</option>
+            <option value="7">Telaga Biru</option>
+            <option value="8">Telawang</option>
+            <option value="9">Teluk Tiram</option>
+            <option value="10">Basirih Selatan</option>
+            <option value="11">Kelayan Barat</option>
+            <option value="12">Kelayan Dalam</option>
+            <option value="13">Kelayan Tengah</option>
+            <option value="14">Kelayan Timur</option>
+            <option value="15">Kelayan Selatan</option>
+            <option value="16">Mantuil</option>
+            <option value="17">Murung Raya</option>
+            <option value="18">Pekauman</option>
+            <option value="19">Pemurus Baru</option>
+            <option value="20">Pemurus Dalam</option>
+            <option value="21">Tanjung Pagar</option>
+            <option value="22">Antasan Besar</option>
+            <option value="23">Tanjung Pagar</option>
+            <option value="24">Antasan Besar</option>
+            <option value="25">Gadang</option>
+            <option value="26">Kertak Baru Ilir</option>
+            <option value="27">Kertak Baru Ulu</option>
+            <option value="28">Kelayan Luar</option>
+            <option value="29">Mawar</option>
+            <option value="30">Melayu</option>
+            <option value="31">Pasar Lama</option>
+            <option value="32">Pekapuran Laut</option>
+            <option value="33">Seberang Masjid</option>
+            <option value="34">Sungai Baru</option>
+            <option value="35">Teluk Dalam</option>
+            <option value="36">Benua Anyar</option>
+            <option value="37">Karang Mekar</option>
+            <option value="38">Kebun Bunga</option>
+            <option value="39">Kuripan</option>
+            <option value="40">Pekapuran Raya</option>
+            <option value="41">Pemurus Luar</option>
+            <option value="42">Pengambangan</option>
+            <option value="43">Sungai Bilu</option>
+            <option value="44">Sungai Lulut</option>
+            <option value="45">Alalak Utara</option>
+            <option value="46">Alalak Tengah</option>
+            <option value="47">Alalak Selatan</option>
+            <option value="48">Antasan Kecil Timur</option>
+            <option value="49">Kuin Utara</option>
+            <option value="50">Pangeran</option>
+            <option value="51">Sungai Andai</option>
+            <option value="52">Sungai Jingah</option>
+            <option value="53">Sungai Miai</option>
+            <option value="54">Sungai Mufti</option>
+          </select>
+          <br><br>
+          
+          <label for="kategori" class="form-label">Kategori Wisata : </label>
+          <input class="form-control" style="color: black" list="kategoriList" id="kategori" placeholder="Type to search...">
+          <datalist id="kategoriList">
+            <option value="Departement Store"></option>
+            <option value="Gereja"></option>
+            <option value="Masjid"></option>
+            <option value="Restoran Non Banjar"></option>
+            <option value="Tempat Bermain Anak"></option>
+            <option value="Taman"></option>
+            <option value="Taman Bermain Air"></option>
+          </datalist>
+          <br>
+
+          <input type="submit" class="btn btn-primary" value="Submit">
+        </form>
+      </div>
     </div>
+  </div>
 </section>
