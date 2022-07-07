b2e5971314d46e6dc8a993575a5f77b17dc53018
imamfarisi19 <edwardindo1@gmail.com>
2022-07-07 17:26:22 +0800

    kategori and kecamatan fixed

--- resources/views/guest/templates/form.blade.php
+++ resources/views/guest/templates/form.blade.php
@@ -11,73 +11,20 @@
           <label for="kategori" class="form-label">Kategori Wisata : </label>
           <input class="form-control" style="color: black" list="kategoriList" id="kategori" name="kategori" placeholder="Ketikan untuk mencari...">
           <datalist id="kategoriList">
-            <option value="Departement Store">
-            <option value="Gereja">
-            <option value="Masjid">
-            <option value="Restoran Non Banjar">
-            <option value="Tempat Bermain Anak">
-            <option value="Taman">
-            <option value="Taman Bermain Air">
+            <option value="Religi">
+            <option value="Kuliner Khas Banjar">
+            <option value="Taman Bermain">
           </datalist>
           <br>
 
-          <label for="kelurahan" class="form-label">Kelurahan Sekarang : </label>
-          <input class="form-control" style="color: black" list="kelurahanList" id="kelurahan" name="kelurahan" placeholder="Ketikan untuk mencari...">
-          <datalist id="kelurahanList">
-            <option value="Basirih">
-            <option value="Belitung Selatan">
-            <option value="Belitung Utara">
-            <option value="Kuin Cerucuk">
-            <option value="Kuin Selatan">
-            <option value="Pelambuan">
-            <option value="Telaga Biru">
-            <option value="Telawang">
-            <option value="Teluk Tiram">
-            <option value="Basirih Selatan">
-            <option value="Kelayan Barat">
-            <option value="Kelayan Dalam">
-            <option value="Kelayan Tengah">
-            <option value="Kelayan Timur">
-            <option value="Kelayan Selatan">
-            <option value="Mantuil">
-            <option value="Murung Raya">
-            <option value="Pekauman">
-            <option value="Pemurus Baru">
-            <option value="Pemurus Dalam">
-            <option value="Tanjung Pagar">
-            <option value="Antasan Besar">
-            <option value="Tanjung Pagar">
-            <option value="Antasan Besar">
-            <option value="Gadang">
-            <option value="Kertak Baru Ilir">
-            <option value="Kertak Baru Ulu">
-            <option value="Kelayan Luar">
-            <option value="Mawar">
-            <option value="Melayu">
-            <option value="Pasar Lama">
-            <option value="Pekapuran Laut">
-            <option value="Seberang Masjid">
-            <option value="Sungai Baru">
-            <option value="Teluk Dalam">
-            <option value="Benua Anyar">
-            <option value="Karang Mekar">
-            <option value="Kebun Bunga">
-            <option value="Kuripan">
-            <option value="Pekapuran Raya">
-            <option value="Pemurus Luar">
-            <option value="Pengambangan">
-            <option value="Sungai Bilu">
-            <option value="Sungai Lulut">
-            <option value="Alalak Utara">
-            <option value="Alalak Tengah">
-            <option value="Alalak Selatan">
-            <option value="Antasan Kecil Timur">
-            <option value="Kuin Utara">
-            <option value="Pangeran">
-            <option value="Sungai Andai">
-            <option value="Sungai Jingah">
-            <option value="Sungai Miai">
-            <option value="Sungai Mufti">
+          <label for="kecamatan" class="form-label">Kecamatan Sekarang : </label>
+          <input class="form-control" style="color: black" list="kecamatanList" id="kecamatan" name="kecamatan" placeholder="Ketikan untuk mencari...">
+          <datalist id="kecamatanList">
+            <option value="Banjarmasin Barat">
+            <option value="Banjarmasin Selatan">
+            <option value="Banjarmasin Tengah">
+            <option value="Banjarmasin Timur">
+            <option value="Banjarmasin Utara">
           </datalist>
           <br>
 
