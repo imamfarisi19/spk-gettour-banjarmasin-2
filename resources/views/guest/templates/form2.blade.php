82988ccfec567a389cbc050a9a0653e8eca9794e
imamfarisi19 <edwardindo1@gmail.com>
2022-07-09 01:07:27 +0800

    fixed form for kriteria

--- resources/views/guest/templates/form2.blade.php
+++ resources/views/guest/templates/form2.blade.php
@@ -8,29 +8,57 @@
       <div class="col-md-7 col-lg-6 text-center text-md-start py-8">
         <form method="post" action="{{route('hasil')}}">
           {{ @csrf_field() }}
-          <label for="" class="form-label">Jarak dari kelurahan sekarang : </label>
-          <input class="form-control" style="color: black" list="kategoriList" id="kategori" placeholder="Ketikan untuk mencari...">
-          <datalist id="kategoriList">
-            <option value="<= 5,9 KM">
-            <option value="6 - 15,9 KM">
-            <option value=">= 16 KM">
-          </datalist>
+
+          <label for="kategori" class="form-label">Kategori Wisata : </label>
+          <select class="form-select" style="color: black" name="kategori" id="kategori">
+            <option disabled>Pilih Kategori</option>
+            <option value="Religi">Religi</option>
+            <option value="Kuliner Khas Banjar">Kuliner Khas Banjar</option>
+            <option value="Taman Bermain">Taman Bermain</option>
+          </select>
           <br>
+          
+          <label for="jarak" class="form-label">Jarak dari kecamatan sekarang : </label>
+          <br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="jarak" id="inlineRadio1" value="1">
+            <label class="form-check-label" for="inlineRadio1"><= 5,9 KM</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="jarak" id="inlineRadio2" value="2" checked>
+            <label class="form-check-label" for="inlineRadio2">6 - 15,9 KM</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="jarak" id="inlineRadio3" value="3">
+            <label class="form-check-label" for="inlineRadio3">>= 16 KM</label>
+          </div>
+          <br><br>
+
+          <label for="area parkir" class="form-label">Area Parkir : </label>
+          <br>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="area parkir" id="inlineRadio4" value="1">
+            <label class="form-check-label" for="inlineRadio4">Tidak Ada Area Parkir</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="area parkir" id="inlineRadio5" value="2" checked>
+            <label class="form-check-label" for="inlineRadio5">Bisa Parkir Sepeda Motor</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="area parkir" id="inlineRadio6" value="3">
+            <label class="form-check-label" for="inlineRadio6">Bisa Parkir Mobil</label>
+          </div>
 
           <label for="kelurahan" class="form-label">Area parkir : </label>
           <input class="form-control" style="color: black" list="kelurahanList" id="kelurahan" placeholder="Ketikan untuk mencari...">
           <datalist id="kelurahanList">
-            <option value="Tidak ada area parkir">
-            <option value="Bisa parkir sepeda motor">
             <option value="Bisa parkir mobil">
+            <option value="Bisa parkir sepeda motor">
+            <option value="Tidak ada area">
           </datalist>
           <br>
-          
-          @foreach($kriteriaTambahanUntukSaws as $value)
-          {{$value->penjelasan}};
-          @endforeach
 
-          <label for="kriteriaKhusus1" class="form-label">Kriteria Khusus 1 : </label>
+          <label for="kriteriaKhusus1" class="form-label">{{ $a }} : </label>
           <input class="form-control" style="color: black" list="kriteriaKhusus1List" id="kriteriaKhusus1" placeholder="Ketikan untuk mencari...">
           <datalist id="kriteriaKhusus1List">
             <option value="sangat penting">
@@ -39,7 +67,7 @@
           </datalist>
           <br>
 
-          <label for="kriteriaKhusus2" class="form-label">Kriteria Khusus 2 : </label>
+          <label for="kriteriaKhusus2" class="form-label">{{ $b }} : </label>
           <input class="form-control" style="color: black" list="kriteriaKhusus2List" id="kriteriaKhusus2" placeholder="Ketikan untuk mencari...">
           <datalist id="kriteriaKhusus2List">
             <option value="sangat penting">
@@ -48,7 +76,7 @@
           </datalist>
           <br>
 
-          <label for="kriteriaKhusus3" class="form-label">Kriteria Khusus 3 : </label>
+          <label for="kriteriaKhusus3" class="form-label">{{ $c }} : </label>
           <input class="form-control" style="color: black" list="kriteriaKhusus3List" id="kriteriaKhusus3" placeholder="Ketikan untuk mencari...">
           <datalist id="kriteriaKhusus3List">
             <option value="sangat penting">
