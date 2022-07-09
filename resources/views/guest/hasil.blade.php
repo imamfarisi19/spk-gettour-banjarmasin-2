747b8dfe3de74f6aeec7e4c9db4ebd03f1b39ecf
imamfarisi19 <edwardindo1@gmail.com>
2022-07-09 23:22:00 +0800

    page for show the result from controller

---
+++ resources/views/guest/hasil.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+<head>
+    @include('guest.templates.head')
+</head>
+
+<body>
+    <main class="main" id="top">
+        @include('guest.templates.navbar')
+        <!-- include('guest.templates.form2') -->
+        <!-- include('guest.templates.mainFooter') -->
+    </main>
+    <!-- include('guest.templates.script') -->
+</body>
+
+</html>
